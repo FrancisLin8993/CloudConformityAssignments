43ca16f7e87ec5d46f0c1473522fd3a2bae5b996
Francis <flin0002@student.monash.edu>
2020-03-20 12:10:52 +1100

    Created cloud conformity api call instance

---
+++ src/apis/apiUtils.js
@@ -0,0 +1,2 @@
+export const CLOUD_CONFORMITY_BASE_URL = "https://ap-southeast-2-api.cloudconformity.com/v1/";
+export const CLOUD_CONFORMITY_CONTENT_TYPE = "application/vnd.api+json";
---
+++ src/apis/cloudConformityApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import CLOUD_CONFORMITY_BASE_URL from './apiUtils';
+import CLOUD_CONFORMITY_CONTENT_TYPE from './apiUtils';
+import apiKeys from './apiKeys';
+
+const cloudConfomrityInstance = axios.create({
+  baseURL: CLOUD_CONFORMITY_BASE_URL
+});
+
+cloudConfomrityInstance.defaults.headers.common['Authorization'] = `ApiKey ${apiKeys}`;
+cloudConfomrityInstance.defaults.headers.common['Content-Type'] = CLOUD_CONFORMITY_CONTENT_TYPE;
+
+export default cloudConfomrityInstance;
