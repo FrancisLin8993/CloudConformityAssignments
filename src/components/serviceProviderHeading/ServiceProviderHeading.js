30128d04eba0c7504ebbb74737e28658a934d441
Francis <flin0002@student.monash.edu>
2020-03-24 09:49:08 +1100

    make service provider heading to be centre of the page

--- src/components/serviceProviderHeading/ServiceProviderHeading.js
+++ src/components/serviceProviderHeading/ServiceProviderHeading.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import awsLogo from '../../assets/logo-square-aws.png';
 import azureLogo from '../../assets/logo-square-azure.png';
-import conformityLogo from '../../assets/logo-square-trend-micro.png';
 import { AWS, AZURE, KNOWLEDGE_BASE_URL } from '../../utils/helpers';
+import './ServiceProviderHeading.css';
 
 class ServiceProviderHeading extends React.Component {
   constructor(props) {
@@ -11,29 +11,29 @@ class ServiceProviderHeading extends React.Component {
   renderImgAndLInk() {
     if (this.props.providerName == AWS) {
       return (
-        <React.Fragment>
+        <div>
           <img alt="Service coverage for Amazon Web Services" src={awsLogo} />
           <br />
           <a href={`${KNOWLEDGE_BASE_URL}${AWS}`}>
             View all {this.props.providerName} rules
           </a>
-        </React.Fragment>
+        </div>
       );
     } else if (this.props.providerName == AZURE) {
       return (
-        <React.Fragment>
+        <div>
           <img alt="Service coverage for Azure Services" src={azureLogo} />
           <br />
           <a href={`${KNOWLEDGE_BASE_URL}${AZURE}`}>
             View all {this.props.providerName} rules
           </a>
-        </React.Fragment>
+        </div>
       );
     }
   }
   render() {
     return (
-      <div>
+      <div class="heading">
         <h3>Service Coverage for</h3>
         {this.renderImgAndLInk()}
       </div>
