353e68e7c9afcd3c8ddf223fa4ea4aa9ac977da1
Francis <flin0002@student.monash.edu>
2020-03-24 17:21:01 +1100

    fixed the layout of content header

--- src/components/contentHeader/ContentHeader.js
+++ src/components/contentHeader/ContentHeader.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import './ContentHeader.css';
 import { KNOWLEDGE_BASE_URL } from '../../utils/helpers';
 
 function ContentHeader() {
   return (
     <div className="content-header grid-section">
-      <div className="jumbotron-view">
-        <Container fluid className="header">
-          <Row className="grid article">
-            <Col className="content-header-col">
-              <h1>Knowledge Base</h1>
-            </Col>
-            <Col className="content-header-col">
-              <p>
-                Cloud Conformity is a continuous assurance tool that provides
-                peace of mind for your cloud infrastructure, delivering over 520
-                automated best practice checks.
-              </p>
-            </Col>
-          </Row>
-          <Row className="grid sitemap">
-            <Col className="wrp">
-              <div className="nav">
-                <span>
-                  <a href={KNOWLEDGE_BASE_URL}>Knowledge Base</a>
-                </span>
+      <div className="table-view">
+        <div className="header">
+          <div className="article">
+            <div className="grid">
+              <div className="column">
+                <h1>Knowledge Base</h1>
               </div>
-            </Col>
-          </Row>
-        </Container>
+              <div className="column">
+                <p>
+                  Cloud Conformity is a continuous assurance tool that provides
+                  peace of mind for your cloud infrastructure, delivering over
+                  520 automated best practice checks.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="sitemap">
+            <div className="grid">
+              <div className="wrp">
+                <div className="nav">
+                  <span>
+                    <a href={KNOWLEDGE_BASE_URL}>Knowledge Base</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/contentHeader/ContentHeader.test.js
+++ src/components/contentHeader/ContentHeader.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ContentHeader from './ContentHeader';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import { shallow, mount } from 'enzyme';
 
 it('renders without crashing', () => {
@@ -10,19 +9,13 @@ it('renders without crashing', () => {
   ReactDOM.render(<ContentHeader />, div);
 });
 
-it('render childrens correctly', () => {
-  const wrapper = shallow(<ContentHeader />);
-  expect(wrapper.find(Row)).toHaveLength(2);
-  expect(wrapper.find(Col)).toHaveLength(3);
-});
-
 it('renders styles correctly', () => {
   const wrapper = shallow(<ContentHeader />);
   expect(wrapper.find('.content-header')).toHaveLength(1);
   expect(wrapper.find('.grid-section')).toHaveLength(1);
-  expect(wrapper.find('.jumbotron-view')).toHaveLength(1);
+  expect(wrapper.find('.table-view')).toHaveLength(1);
   expect(wrapper.find('.header')).toHaveLength(1);
   expect(wrapper.find('.grid')).toHaveLength(2);
-  expect(wrapper.find('.content-header-col')).toHaveLength(2);
+  expect(wrapper.find('.column')).toHaveLength(2);
   expect(wrapper.find('.article')).toHaveLength(1);
 });
