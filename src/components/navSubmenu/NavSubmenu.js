c76865ec9cad404046c2a527c462b08fa4270276
Francis <flin0002@student.monash.edu>
2020-03-26 16:14:18 +1100

    added an icomoon icon beside the solutions tab in the navbar

--- src/components/navSubmenu/NavSubmenu.js
+++ src/components/navSubmenu/NavSubmenu.js
@@ -7,7 +7,10 @@ import './NavSubmenu.css';
 function NavSubmenu() {
   return (
     <React.Fragment>
-      <span> Solutions</span>
+      <span>
+        {' '}
+        Solutions <i class="icomoon icon-arrow-down2"></i>
+      </span>
       {/* The arrow icon in icomoon is not free so that i am not using icons here */}
       <div className="submenu">
         <div className="submenu-container">
